b22171d593d9fc5d15c4135804dae28524b07040
Donia-Kareem <147209045+Donia-Kareem@users.noreply.github.com>
2024-10-28 22:01:08 +0300

    Update RmdirCommand.java

--- src/main/java/org/os/RmdirCommand.java
+++ src/main/java/org/os/RmdirCommand.java
@@ -4,40 +4,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class RmdirCommand {
-    public void rmdir() {
-        Scanner scanner = new Scanner(System.in);
+    public String rmdir(String dirPathStr) {
+        Path dirPath = Paths.get(dirPathStr); 
 
-        // Prompt the user to enter the directory name
-        System.out.print("Enter the directory you want to remove: ");
-        String dirName = scanner.nextLine();
-        // Get the current working directory
-        String currentDirectory = System.getProperty("user.dir");
-        Path dirPath = Paths.get(currentDirectory, dirName);
         // Check if the specified directory exists
         if (!Files.exists(dirPath)) {
-            System.out.println("Directory does not exist: " + dirName);
-            return;
+            return "Directory does not exist: " + dirPathStr;
         }
-        // Verify that the specified path is a directory (not a file)
+
+        // Verify that the specified path is a directory
         if (!Files.isDirectory(dirPath)) {
-            System.out.println(dirName + " is not a directory.");
-            return;
+            return dirPathStr + " is not a directory.";
         }
 
         try {
             // Check if the directory is empty
             if (Files.newDirectoryStream(dirPath).iterator().hasNext()) {
-                System.out.println(dirName + " is not empty.");
+                return dirPathStr + " is not empty.";
             } else {
                 // Delete the directory if it's empty
                 Files.delete(dirPath);
-                System.out.println("Removed directory: " + dirName);
+                return "Removed directory: " + dirPathStr;
             }
         } catch (IOException e) {
-            System.out.println("Error removing directory: " + e.getMessage());
+            return "Error removing directory: " + e.getMessage();
         }
     }
 }
