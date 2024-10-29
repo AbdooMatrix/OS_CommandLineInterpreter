13f5eea87b450b5a5d25a26f5d66dac0d4999c08
Abdelrahman Mostafa <114619776+AbdooMatrix@users.noreply.github.com>
2024-10-29 12:54:11 +0300

    converted if statement in the main to be switch statement

--- src/main/java/org/os/CommandLineInterpreter.java
+++ src/main/java/org/os/CommandLineInterpreter.java
@@ -10,6 +10,7 @@ public class CommandLineInterpreter {
 
         String currDirec = new PWDCommand().pwd();
 
+        label:
         while (true) {
             // Always printing the current working directory.
             System.out.print(currDirec + " --> ");
@@ -27,57 +28,59 @@ public class CommandLineInterpreter {
             String command = tokens[0];
             String output = "";
 
-            if ("exit".equals(command)) {
-                System.out.println("Exiting CLI.");
-                break;
-            }
-            else if ("help".equals(command)) {
-                output = getHelpText();
-            }
-            else if ("ls".equals(command)) {
-                // Handle "ls" commands with options
-                if (tokens.length == 1) {
-                    output = LsCommand.listDirectory(currDirec);
-                } else if ("-a".equals(tokens[1])) {
-                    output = LsCommand.listAllFiles(tokens.length > 2 ? tokens[2] : currDirec);
-                } else if ("-r".equals(tokens[1])) {
-                    output = LsCommand.listFilesReversed(tokens.length > 2 ? tokens[2] : currDirec);
-                } else if (tokens.length == 2) {
-                    output = LsCommand.listDirectory(tokens[1]);
-                } else {
-                    output = "Error: Unsupported 'ls' option. Supported options: '-a' (all files), '-r' (reverse order).\n";
-                }
-            }
-            else if ("mv".equals(command)) {
-                String srcPath = tokens[1], destPath = tokens[2];
-                MoveCommand mv = new MoveCommand();
-                mv.move(srcPath, destPath);
-            }
-            else if ("touch".equals(command)) {
-                output = new TouchCommand().createOrUpdateFile(tokens.length > 1 ? tokens[1] : null);
-            }
-            else if ("cat".equals(command)) {
-                String[] filePaths = new String[tokens.length - 1];
-                for (int i = 1; i < tokens.length; i++) {
-                    filePaths[i - 1] = tokens[i].trim();
-                }
-                output = new CatCommand().cat(filePaths);
-            }
-            else if ("cd".equals(command)) {
-                currDirec = new cdCommand().cd(tokens.length > 1 ? tokens[1] : null);
-            }
-            else if ("mkdir".equals(command)) {
-                output = new Mkdir().execute(tokens.length > 1 ? tokens[1] : currDirec);
-            }
-            else if ("pipe".equals(command)) {
-                StringBuilder commandline = null;
-                for (int i = 1; i < tokens.length; i++) {
-                    commandline.append(tokens[i]).append(" | ");
-                }
-                PipeCommand pipe = new PipeCommand();
-            }
-            else {
-                output = "Error: Command not recognized. Type 'help' for a list of commands.\n";
+            switch (command) {
+                case "exit":
+                    System.out.println("Exiting CLI.");
+                    break label;
+                case "help":
+                    output = getHelpText();
+                    break;
+                case "ls":
+                    // Handle "ls" commands with options
+                    if (tokens.length == 1) {
+                        output = LsCommand.listDirectory(currDirec);
+                    } else if ("-a".equals(tokens[1])) {
+                        output = LsCommand.listAllFiles(tokens.length > 2 ? tokens[2] : currDirec);
+                    } else if ("-r".equals(tokens[1])) {
+                        output = LsCommand.listFilesReversed(tokens.length > 2 ? tokens[2] : currDirec);
+                    } else if (tokens.length == 2) {
+                        output = LsCommand.listDirectory(tokens[1]);
+                    } else {
+                        output = "Error: Unsupported 'ls' option. Supported options: '-a' (all files), '-r' (reverse order).\n";
+                    }
+                    break;
+                case "mv":
+                    String srcPath = tokens[1], destPath = tokens[2];
+                    MoveCommand mv = new MoveCommand();
+                    mv.move(srcPath, destPath);
+                    break;
+                case "touch":
+                    output = new TouchCommand().createOrUpdateFile(tokens.length > 1 ? tokens[1] : null);
+                    break;
+                case "cat":
+                    String[] filePaths = new String[tokens.length - 1];
+                    for (int i = 1; i < tokens.length; i++) {
+                        filePaths[i - 1] = tokens[i].trim();
+                    }
+                    output = new CatCommand().cat(filePaths);
+                    break;
+                case "cd":
+                    currDirec = new cdCommand().cd(tokens.length > 1 ? tokens[1] : null);
+                    break;
+                case "mkdir":
+                    output = new Mkdir().execute(tokens.length > 1 ? tokens[1] : currDirec);
+                    break;
+                case "pipe":
+                    StringBuilder commandline = null;
+                    for (int i = 1; i < tokens.length; i++) {
+                        commandline.append(tokens[i]).append(" | ");
+                    }
+                    PipeCommand pipe = new PipeCommand();
+                    break;
+                case null:
+                default:
+                    output = "Error: Command not recognized. Type 'help' for a list of commands.\n";
+                    break;
             }
 
             // Redirect output if ">" is specified; otherwise, print to console
