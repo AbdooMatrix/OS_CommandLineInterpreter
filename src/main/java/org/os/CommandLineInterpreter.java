b18c76f8f41daca2d2e1f282ee6403525638c9dd
Abdelrahman Mostafa <114619776+AbdooMatrix@users.noreply.github.com>
2024-10-30 20:44:38 +0300

    added help class and its testing and edited in the main.

--- src/main/java/org/os/CommandLineInterpreter.java
+++ src/main/java/org/os/CommandLineInterpreter.java
@@ -11,6 +11,7 @@ public class CommandLineInterpreter {
         System.out.println("Welcome to the CLI. Type 'exit' to quit.");
 
         String currDir = PWDCommand.getInstance().getCurrentDirec();
+        HelpCommand helpCommand = new HelpCommand(); // Create instance of HelpCommand
 
         loop:
         while (true) {
@@ -52,7 +53,7 @@ public class CommandLineInterpreter {
                     break loop;
 
                 case "help":
-                    output = getHelpText();
+                    output = helpCommand.getHelpText(); // Call HelpCommand instance
                     break;
 
                 case "ls":
@@ -156,55 +157,6 @@ public class CommandLineInterpreter {
         }
         return new CatCommand().cat(filePaths);
     }
-
-    public static String getHelpText() {
-        return """
-       Available commands:
-       - help: Displays this help message.
-           Example: help
-       
-       - exit: Exits the CLI.
-           Example: exit
-       
-       - ls [path]: Lists files in the specified directory (default is current directory).
-           Example: ls
-                    ls D:\\Folder
-
-       - ls -a [path]: Lists all files, including hidden ones, in the specified directory.
-           Example: ls -a
-                    ls -a D:\\Folder
-
-       - ls -r [path]: Lists files in reverse order in the specified directory.
-           Example: ls -r
-                    ls -r D:\\Folder
-
-       - touch <file>: Creates a new file or updates the timestamp of an existing file.
-           Example: touch newFile.txt
-
-       - mv [sourcePath] [destinationPath]: Moves a file from one directory to another.
-           Example: mv D:\\Folder\\file.txt D:\\NewFolder
-
-       - rm <file_path>: Removes a file or directory.
-           Example: rm D:\\Folder\\file.txt
-
-       - cat <file_path(s)>: Concatenates and displays the content of files.
-           Example: cat file1.txt file2.txt
-
-       - cd <directory>: Changes the current directory.
-           Example: cd D:\\Folder
-
-       - mkdir <directory>: Creates a new directory.
-           Example: mkdir newFolder
-       
-       Redirection Operators:
-       - [command] > [file]: Redirects command output to a file, overwriting existing content.
-           Example: ls > output.txt
-
-       - [command] >> [file]: Appends command output to a file without overwriting.
-           Example: ls >> output.txt
-       """;
-    }
-
 
     private static List<String> parseCommandInput(String commandInput) {
         List<String> tokens = new ArrayList<>();
