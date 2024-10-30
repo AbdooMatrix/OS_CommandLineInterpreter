8de12ce091aadc2d26009e38e27f03a51836a6eb
asmaa465 <114663258+asmaa465@users.noreply.github.com>
2024-10-31 00:05:04 +0300

    Add files via upload

--- src/main/java/org/os/CdCommand.java
+++ src/main/java/org/os/CdCommand.java
@@ -3,28 +3,41 @@ package org.os;
 import java.io.File;
 
 public class CdCommand {
-    static String cw = System.getProperty("user.dir");
+
+    public static String cw;
+
 
     public static String cd(String path) {
-        if (path.equals("..")) {
-            File current = new File(cw);
-            if (current.getParent() != null) {  // Prevent going above root
-                cw = current.getParent();
-            } else {
-                return "cannot go above root directory.";
-            }
-        } else if (path.equals(".")) {
-            return cw;
-        } else {
-            File dir = new File(cw, path);
+        if (path.matches("^[a-zA-Z]:\\\\.*")) {
+            File dir = new File(path);
             if (dir.exists() && dir.isDirectory()) {
                 cw = dir.getAbsolutePath();
-            } else if (!dir.exists()) {
-                return "Incorrect path: " + path;
             } else {
-                return "not a directory: " + path;
+                return "Incorrect path: " + path;
+            }
+        } else {
+            File current = new File(cw);
+            if (path.equals("..")) {
+                if (current.getParent() != null) {
+                    cw = current.getParent();
+                } else {
+                    return "Cannot go above root directory.";
+                }
+            } else if (path.equals(".")) {
+                return cw;
+            } else {
+                File dir = new File(current, path);
+                if (dir.exists() && dir.isDirectory()) {
+                    cw = dir.getAbsolutePath();
+                } else if (!dir.exists()) {
+                    return "Incorrect path: " + path;
+                } else {
+                    return "Not a directory: " + path;
+                }
             }
         }
-        return (cw);
+        return cw;
     }
+
 }
+
